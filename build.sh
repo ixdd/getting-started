418d1c297da98677ae33ba009ae87effd9f91c57
Ahmed Ganatra <ahganatr@outlook.com>
2022-05-21 09:58:45 -0500

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,14 +1,14 @@
-#!/bin/bash
+# #!/bin/bash
 
-set -e
+# set -e
 
-if [ $1 == "--push" ]; then
-    WILL_PUSH=1
-else
-    WILL_PUSH=0
-fi
+# if [ $1 == "--push" ]; then
+#     WILL_PUSH=1
+# else
+#     WILL_PUSH=0
+# fi
 
-docker buildx build \
-      --platform linux/amd64,linux/arm64 \
-      -t docker/getting-started:latest \
-      $( (( $WILL_PUSH == 1 )) && printf %s '--push' ) .
+# docker buildx build \
+#       --platform linux/amd64,linux/arm64 \
+#       -t docker/getting-started:latest \
+#       $( (( $WILL_PUSH == 1 )) && printf %s '--push' ) .
